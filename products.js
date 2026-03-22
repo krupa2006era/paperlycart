e901f9e3dbb88c04805e286e6de3d3ca45b26f03
krupa2006era <krupagoka2026@gmail.com>
2026-03-22 16:56:59 +0530

    Updated products and images

--- products.js
+++ products.js
@@ -22,18 +22,18 @@ const products = [
   { name: "OX GLOW BALL PENS", price: 75, pieces: 10, category: "Pens",image:"images/xoglow.png" },
   { name: "COPPER TOUCH PENS", price: 70, pieces: 10, category: "Pens",image:"images/coppertouch.png" },
   { name: "RADIUM PENS", price: 50, pieces: 20, category: "Pens",image:"images/lky.png" },
-  { name: "ADD GEL PENS", price: 80, pieces: 100, category: "Pens",image:"images/addgel.png"},
+  { name: "ADD GEL PENS", price: 10, pieces: 10, category: "Pens",image:"images/addgel.png"},
   { name: "FLAIR NEO PENS", price: 70, pieces: 10, category: "Pens",image:"images/flairneo.png" },
+  { name: "BRITE PENS", price: 20, pieces: 5, category: "Pens",image:"images/br.png" },
   
 
-  { name: "APSARA TIDY ERASERS", price: 200, pieces: 100, category: "Erasers",image:"images/tidy.png" },
-  { name: "APSARA STRIPS ERASERS", price: 100, pieces: 100, category: "Erasers",image:"images/st.png" },
-  { name: "APSARA ERASERS", price: 250, pieces: 100, category: "Erasers",image:"images/ap.png" },
-  { name: "DOMS (M-TECH ERASERS)", price: 240, pieces: 375, category: "Erasers",image:"images/tech.png" },
+  { name: "APSARA TIDY ERASERS", price: 200, pieces: 20, category: "Erasers",image:"images/tidy.png" },
+  { name: "APSARA STRIPS ERASERS", price: 100, pieces: 10, category: "Erasers",image:"images/st.png" },
+  { name: "APSARA ERASERS", price: 250, pieces: 50, category: "Erasers",image:"images/ap.png" },
+  { name: "DOMS (M-TECH ERASERS)", price: 240, pieces: 24, category: "Erasers",image:"images/tech.png" },
   { name: "DOMS ERASERS", price: 42, pieces: 10, category: "Erasers",image:"images/er.png"},
-  { name: "LIPSTICK ERASERS", price: 16, pieces: 2, category: "Erasers" },
+  { name: "LIPSTICK ERASERS", price: 16, pieces: 2, category: "Erasers",image:"images/lip.png" },
   { name: "TIRE ERASERS", price: 175, pieces: 40, category: "Erasers",image:"images/tt.png" },
-  { name: "APSARA ERASERS", price: 200, pieces: 50, category: "Erasers" },
   {name: "APSARA  MARVEL ERASERS", price: 200, pieces: 25, category: "Erasers",image:"images/apsaramarvel.png" },
   { name: "DOMES CAR ERASERS", price: 100, pieces: 12, category: "Erasers",image:"images/car.png" },
 
@@ -47,17 +47,17 @@ const products = [
   { name: "APSARA SKETCHES", price: 22, pieces: 1, category: "Sketches",image:"images/apa.png" },
   { name: "DOMS SKETCHES", price: 44, pieces: 4, category: "Sketches",image:"images/domss.png" },
 
-  { name: "DOMS PAINTING", price: 75, pieces: 1, category: "Paint" },
+  
   { name: "DOMS COLOURS", price: 99, pieces: 6, category: "Paint",image:"images/domsshades.png" },
 
   { name: "GLUE STICKS", price: 195, pieces: 24, category: "Glue",image:"images/gluestick.png" },
   { name: "LION GUM", price: 150, pieces: 30, category: "Glue" },
-  { name: "FEVICOL", price: 400, pieces: 40, category: "Glue" },
+  { name: "FEVICOL", price: 400, pieces: 40, category: "Glue",image:"images/fe.png" },
 
-  { name: "KEY CHAINS", price: 70, pieces: 12, category: "Accessory",image:"images/keychains.png" },
-  { name: "PAPER SOAPES", price: 480, pieces: 80, category: "Soap papers",image:"images/soap.png" },
+  { name: "KEY CHAINS", price: 120, pieces: 12, category: "Accessory",image:"images/keychains.png" },
+  { name: "PAPER SOAPES", price: 480, pieces: 24, category: "Soap papers",image:"images/soap.png" },
 
-  { name: "SPIRAL BOOK", price: 100, pieces: 300, category: "Books" },
+
   { name: "POUCHES", price: 200, pieces: 20, category: "Pouches" },
 
 
